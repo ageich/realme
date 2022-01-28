3a895a46e48dc2081e247c6f43f4794777fe9d9a
Sarthak Roy <sarthakroy2002@gmail.com>
2022-01-28 05:13:18 +0000

    r5x: blobs: Copy Moto troika fp hal and egis tee to fptmp
    *https://github.com/sarthakroy2002/android_device_realme_r5x/commit/a7a7a0dbe7b5ceb866f54b97cb59d01a005a9dc3
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- r5x-vendor.mk
+++ r5x-vendor.mk
@@ -605,6 +605,8 @@ PRODUCT_COPY_FILES += \
     vendor/realme/r5x/proprietary/vendor/firmware/tp/19631/nt_hlt_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/tp/19631/nt_hlt_fw.bin \
     vendor/realme/r5x/proprietary/vendor/firmware/tp/19631/nt_hlt_fw_mp.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/tp/19631/nt_hlt_fw_mp.bin \
     vendor/realme/r5x/proprietary/vendor/firmware/tp/19631/nt_hlt_fw_signed.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/tp/19631/nt_hlt_fw_signed.bin \
+    vendor/realme/r5x/proprietary/vendor/fptmp/libets_teeclient_v2.so:$(TARGET_COPY_OUT_VENDOR)/fptmp/libets_teeclient_v2.so \
+    vendor/realme/r5x/proprietary/vendor/fptmp/android.hardware.biometrics.fingerprint@2.1-service.r5x:$(TARGET_COPY_OUT_VENDOR)/fptmp/android.hardware.biometrics.fingerprint@2.1-service.r5x \
     vendor/realme/r5x/proprietary/vendor/lib/android.hardware.camera.provider@2.4-external.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.camera.provider@2.4-external.so \
     vendor/realme/r5x/proprietary/vendor/lib/android.hardware.camera.provider@2.4-legacy.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.camera.provider@2.4-legacy.so \
     vendor/realme/r5x/proprietary/vendor/lib/android.hardware.sensors@2.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.sensors@2.0-impl.so \
